561f96793d51723d2253253b9e2a1b73a280c4aa
belgianwafflecorp <domien.verstraete@student.vives.be>
2024-04-28 12:55:46 +0200

    start files

---
+++ QuestionnaireLibrary/Answer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireLibrary
+{
+    internal class Answer
+    {
+
+        /*
+        + Text : string 
+        + IsCorrect : bool
+
+        + Answer(text: string, IsCorrect: bool)
+        + ToString() : string
+        */
+
+        public string text = null;
+        public bool isCorrect = false;
+
+        public Answer(string text, bool isCorrect)
+        {
+            this.text = Text;
+            this.isCorrect = IsCorrect;
+        }
+
+        public override string ToString()
+        {
+            return Text + "is " + IsCorrect;
+        }
+    }
+}
---
+++ QuestionnaireLibrary/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireLibrary
+{
+    internal class Question
+    {
+
+        private List<Question> possibleAnswer;
+
+        // + <<get/set>> Text: string
+        
+
+    }
+}
