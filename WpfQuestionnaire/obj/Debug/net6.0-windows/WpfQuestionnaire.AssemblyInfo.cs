1e961257b9d058f0d8a5443ee74c76129e158ad2
Domien Verstraete <verstraeted@protonmail.com>
2024-05-14 11:36:39 +0200

    post merge

--- WpfQuestionnaire/obj/Debug/net6.0-windows/WpfQuestionnaire.AssemblyInfo.cs
+++ WpfQuestionnaire/obj/Debug/net6.0-windows/WpfQuestionnaire.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfQuestionnaire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46548121e9493235dddebfe336d02d2d08926b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebdeff9992ae3f39834ac4e826d7a09c39dedd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfQuestionnaire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfQuestionnaire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
